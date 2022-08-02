63340be4d534078cd49db9b632f856e0af41f2b8
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-08-02 18:29:01 -0300

    Create game.js

---
+++ js/data/game.js
@@ -0,0 +1,12 @@
+var game = {
+  layers:[{
+    startData:{
+      unlocked:true,
+      resource:new Decimal(0)
+    },
+    name:"Mass",
+    layerId:0,
+    layerShown() {return true},
+  }],
+  changelog:new Changelog("Changelog", changelog)
+}
